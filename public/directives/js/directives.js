9a01e710dd7e0fb8f75d1dada03c3d23658a1111
Jeremy Greer <jeremy.greer.atl@gmail.com>
2015-01-19 21:30:22 -0500

    finish date directiev

--- public/directives/js/directives.js
+++ public/directives/js/directives.js
@@ -7,15 +7,30 @@
     return {
       restrict: 'E',
 
+      scope: {
+        dateFormat: '@?format',
+        interval: '@?interval'
+      },
+
       controller: ['$scope', function ($scope) {
         $scope.date = new Date();
-
-        setInterval(function () {
-          $scope.date = new Date();
-        }, 1000);
       }],
 
-      templateUrl: '/directives/templates/date.html'
+      templateUrl: '/directives/templates/date.html',
+
+      link: function (scope, iElement, iAttrs, controller, transcludeFn) {
+
+        scope.dateFormat = scope.dateFormat || 'medium';
+        scope.interval = (parseInt(scope.interval, 10) || 1) * 1000;
+
+        setInterval(function () {
+          scope.date = new Date();
+
+          // Since we changed the scope's value without Angular
+          // knowing, we have to trigger the digest loop with $apply.
+          scope.$apply();
+        }, scope.interval);
+      }
     };
   });
 
